ec32a18f1e9cee60a1eeaf2f23f19fe58d41bcf0
Ken Devane <kendevane@10-4-231-115.vlan890.client.wireless.uic.edu>
2015-11-02 11:23:16 -0600

    Scaled Points Class Idea

---
+++ src/ScaledPoint.java
@@ -0,0 +1,26 @@
+public class ScaledPoint
+{
+    private int x;
+    private int y;
+    
+    public ScaledPoint()
+    {
+        this(0,0);
+    }
+    
+    public ScaledPoint(int x, int y)
+    {
+        this.x=0;
+        this.y=y;
+    }
+    
+    public static int scaledX(int currentX, double scaleFactor)
+    {
+        return (int)(scaleFactor*currentX);
+    }
+    
+    public static int scaledY(int currentY, double scaleFactor)
+    {
+        return (int)(scaleFactor*currentY);
+    }
+}
