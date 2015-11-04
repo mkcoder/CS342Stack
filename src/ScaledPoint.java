87e443b7890f116c63cb2771590bf852310e1190
Ken Devane <kendevane@dhcp-vlan861-9-212-91.vlan861.client.wireless.uic.edu>
2015-11-04 10:13:36 -0600

    Added Comments

--- src/ScaledPoint.java
+++ src/ScaledPoint.java
@@ -1,25 +1,36 @@
+//
+// Class to facilitate re-sizing of an applet
+//
+
 public class ScaledPoint
 {
-    private int x;
-    private int y;
+    private int x;              // stores a scaled x coordinate
+    private int y;              // stores a scaled y coordinate
     
     public ScaledPoint()
+    //POST: creates an object with coordinate (0,0)
     {
         this(0,0);
     }
     
     public ScaledPoint(int x, int y)
+    //PRE:  x > 0, y > 0
+    //POST: creates an object with coordinate (x,y)
     {
         this.x=x;
         this.y=y;
     }
     
     public static int scaledX(int currentX, double scaleFactor)
+    //PRE:  currentX > 0, 0 < scaleFactor < 1
+    //POST: FCTVAL == the scaled x coordinate based on scaleFactor
     {
         return (int)(scaleFactor*currentX);
     }
     
     public static int scaledY(int currentY, double scaleFactor)
+    //PRE:  currentY > 0, 0 < scaleFactor < 1
+    //POST: FCTVAL == the scaled y coordinate based on scaleFactor
     {
         return (int)(scaleFactor*currentY);
     }
