7f604fe8eb2c6ae3fa6693cde28d070c8dc89e80
Chris Griffith <cgriffith792@gmail.com>
2015-11-10 21:11:33 -0600

    Added arrow to drawing

--- src/Box.java
+++ src/Box.java
@@ -1,4 +1,4 @@
-// Programmers: Chris Griffith, Oliver San Juan, Muhammad, Ken Devane
+// Programmers: Chris Griffith, Oliver San Juan, Muhammad K. Khan, Ken Devane
 // Assignment:  Project 3, Data Structure Visualization
 // Date:        November 4, 2015
 // Description: Class which holds information about stack drawing
@@ -8,12 +8,16 @@ import java.awt.Color;
 
 public class Box
 {
-    protected ScaledPoint stack;              // Contains scaled coordinates for drawing
-    protected Color       color;              // Default color of stack outline
+    private static final int XOFFSET = 40;      // X coordinate offset for stack labels
+    private static final int YOFFSET = 150;     // Y coordinate offset for stack labels
+    private static final int TWENTYFIVE = 25;   // Arrow head ratio
+    
+    protected ScaledPoint stack;                // Contains scaled coordinates for drawing
+    protected Color       color;                // Default color of stack outline
 
     public Box()
-    //POST: creates a box object with starting location at (0.25, 0.25),
-    //      width set to 0.25, height set to 0.75, and color set to BLACK
+    //POST: creates a box object with starting location at (0.10, 0.20),
+    //      width set to 0.40, height set to 0.75, and color set to BLACK
     {
         this.stack = new ScaledPoint(0.10, 0.20, 0.40, 0.75);
         
@@ -23,22 +27,9 @@ public class Box
     public Box(Graphics g, int appWidth, int appHeight)
     //PRE:  g is initialized, appWidth > 0, and appHeight > 0
     //POST: creates a box object and draws this box a with starting 
-    //      location at (0.25, 0.25), width set to 0.25, height 
+    //      location at (0.10, 0.20), width set to 0.40, height 
     //      set to 0.75, and color set to BLACK
     {
-    	// TODO: adding bottom arrow with label
-    	// you have the bottom of the stack which would be
-    	// X=0.10, y=0.20, W=0.40, H=0.75
-    	// 
-    	//	(X,Y)
-    	//  [     W      ]  <- <= (1,1+W+PADDING_TO_THE_RIGHT_OF_THE_STACK) 
-    	//
-    	//
-    	//		  H       
-    	//
-    	//
-    	//  [     W      ]  <- <= (1,Y+W+PADDING_TO_THE_RIGHT_OF_THE_STACK)    	
-    	//                   
         this.stack = new ScaledPoint(0.10, 0.20, 0.40, 0.75);        
         color = Color.BLACK;
 
@@ -48,8 +39,6 @@ public class Box
         int height = stack.scaledHeight(appHeight);
         g.setColor(color);
         g.drawRect(x, y, width, height);
-        
-        
     }
 
     public void redraw(Graphics g, int appWidth, int appHeight)
@@ -66,11 +55,25 @@ public class Box
 
         g.setColor(color);
         System.out.println("I WAS CALLED");
-        
+
+        //Draw label and arrow to show how the stack grows
+        g.drawString("Stack Grows", x+width+XOFFSET, (y+(height/2))+10);
+        g.drawLine(x+width+TWENTYFIVE, (y+(height/2))+TWENTYFIVE, 
+                   x+width+TWENTYFIVE, (y+(height/2)-TWENTYFIVE));
+
+        //Draw arrow head
+        g.drawLine(x+width+TWENTYFIVE, (y+(height/2))-TWENTYFIVE, x+width+15, (y+(height/2)));
+        g.drawLine(x+width+TWENTYFIVE, (y+(height/2))-TWENTYFIVE, x+width+35, (y+(height/2)));
+
+        //Draw line showing the bottom of the stack
+        g.drawLine(x+width+10, y+height, x+width+YOFFSET, y+height);
+        g.drawString("Stack Bottom",x+width+XOFFSET, y+height);
+
         //Draw line showing the bottom of the stack
         g.drawLine(x+width+10, y+height, x+width+150, y+height);
         g.drawString("Stack Bottom",x+width+40, y+height);
-        
+
+        //Draw stack outline
         g.drawRect(x, y, width, height); 
     }
 }
